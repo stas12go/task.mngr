42ff9934aa9be795f14ea8756db02bc2749a90a2
stas12go <van4ugovstas@gmail.com>
2021-05-25 12:22:00 +0500

    set mysql connection

--- config/db_params.php
+++ config/db_params.php
@@ -1,9 +1,18 @@
 <?php
 
 // Массив с параметрами подключения к базе данных
-return array(
-    'host' => 'localhost',
-    'dbname' => 'task_manager',
-    'user' => 'root',
-    'password' => 'root',
-);
+if ($_SERVER['SERVER_NAME'] == 'stas12go-task-mngr.herokuapp.com') {
+	return array(
+	    'host' => 'localhost',
+	    'dbname' => 'task_manager',
+	    'user' => 'root',
+	    'password' => 'root',
+	);
+} else {
+	return array(
+	    'host' => 'eu-cdbr-west-01.cleardb.com',
+	    'dbname' => 'heroku_32cce09e8b8a549',
+	    'user' => 'b06d8cb41d747c',
+	    'password' => '6fb15815',
+	);
+}
