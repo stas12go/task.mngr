fc225a7848fd8fe59d50b73cc2d9ee85745c6c41
stas12go <van4ugovstas@gmail.com>
2021-05-25 12:39:44 +0500

    set mysql connection

--- components/Db.php
+++ components/Db.php
@@ -17,7 +17,20 @@ class Db
         $params = include($paramsPath);
 
         // Устанавливаем соединение
-        $db = new PDO("mysql:host={$params['host']};dbname={$params['dbname']}", $params['user'], $params['password']);
+        if ($_SERVER['SERVER_NAME'] == "stas12go-task-mngr.herokuapp.com") {
+            //Get Heroku ClearDB connection information
+            $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $cleardb_server = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db = substr($cleardb_url["path"],1);
+            $active_group = 'default';
+            $query_builder = TRUE;
+            // Connect to DB
+            $db = new PDO($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        } else {
+            $db = new PDO("mysql:host={$params['host']};dbname={$params['dbname']}", $params['user'], $params['password']);
+        }
 
         // Задаем кодировку
         $db->exec("set names utf8");
