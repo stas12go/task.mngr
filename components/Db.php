c84878ade2a6b9e841749657b2716dd7cc4c55a0
stas12go <van4ugovstas@gmail.com>
2021-05-25 12:41:28 +0500

    set mysql connection

--- components/Db.php
+++ components/Db.php
@@ -27,7 +27,7 @@ class Db
             $active_group = 'default';
             $query_builder = TRUE;
             // Connect to DB
-            $db = new PDO($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+            $db = new PDO("mysql:host={$cleardb_server};dbname={$cleardb_username}", $cleardb_password, $cleardb_db);
         } else {
             $db = new PDO("mysql:host={$params['host']};dbname={$params['dbname']}", $params['user'], $params['password']);
         }
